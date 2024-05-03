04d0c069445a4a0f691fe7e9041e66c4121f248e
leah <leah@pronounmail.com>
2024-05-04 00:23:16 +0100

    add opengraph stuff

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
     
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 

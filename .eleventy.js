df15a0a7607168e49b47eb82c5cadb15ea8f15ca
leah <leah@pronounmail.com>
2024-05-03 17:37:32 +0100

    prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const yaml = require("js-yaml");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
     eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 }

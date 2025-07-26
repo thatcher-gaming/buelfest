1fd295b105d23e48e7dc813960d199938034a203
Milo Szecket <miloszecket@gmail.com>
2025-07-26 19:19:43 -0400

    Update donate.11tydata.js
    testing a solution that for sure will not work

--- donate/donate.11tydata.js
+++ donate/donate.11tydata.js
@@ -2,15 +2,15 @@ const eleventyFetch = require("@11ty/eleventy-fetch");
 
 const cool_url = "https://leah.pronounmail.com/buelfest/donations.php";
 
-module.exports = {
-    eleventyComputed: {
-        donations: async () => {
-            const res = await eleventyFetch(cool_url, {
-                duration: "5m",
-                type: "json",
-            });
+// module.exports = {
+//     eleventyComputed: {
+//         donations: async () => {
+//             const res = await eleventyFetch(cool_url, {
+//                 duration: "5m",
+//                 type: "json",
+//             });
     
-            return res;
-        }
-    }
-}
+//             return res;
+//         }
+//     }
+// }

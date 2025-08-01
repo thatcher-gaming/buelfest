6325c7cbfff1037862ce264da8da875aeda0715e
Milo Szecket <miloszecket@gmail.com>
2025-08-01 19:29:02 -0400

    Update donate.11tydata.js
    for push

--- donate/donate.11tydata.js
+++ donate/donate.11tydata.js
@@ -3,14 +3,14 @@ const eleventyFetch = require("@11ty/eleventy-fetch");
 const cool_url = "https://leah.pronounmail.com/buelfest/donations.php";
 
 module.exports = {
-    eleventyComputed: {
-        donations: async () => {
-            const res = await eleventyFetch(cool_url, {
-                duration: "5m",
-                type: "json",
-            });
+    // eleventyComputed: {
+    //     donations: async () => {
+    //         const res = await eleventyFetch(cool_url, {
+    //             duration: "5m",
+    //             type: "json",
+    //         });
     
-            return res;
-        }
-    }
+    //         return res;
+    //     }
+    // }
 }
